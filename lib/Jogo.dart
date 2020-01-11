0fd91aa2fc973b9c80ac047ba664f9c16a2542e5
junim2307@gmail.com <130913at>
2020-01-11 15:55:59 -0300

    Initial commit

---
+++ lib/Jogo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
